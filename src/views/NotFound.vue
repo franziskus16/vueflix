3390ebcf243b6844b7547567f9f03c724c5df1eb
Francesc Gimenez Gil <fgimgil@gmail.com>
2025-03-20 11:56:02 +0100

    new sass file for not found page with specific styles

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,40 +7,5 @@
 </template>
 
 <style scoped lang="scss">
-.error-message {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: $light-text;
-  min-height: 80vh;
-  h1 {
-    font-size: 45px;
-  }
-  p {
-    font-size: 32px;
-  }
-  .btn-back {
-    position: relative;
-    width: 180px;
-    height: 60px;
-    cursor: pointer;
-    background: transparent;
-    border: 1px solid #adff91;
-    box-sizing: border-box;
-    outline: none;
-    transition: 20s ease-in-out;
-    color: $light-text;
-    text-decoration: none;
-    margin: 0 auto;
-    padding: 0;
-    display: inline-block;
-    line-height: 50px;
-    text-align: center;
-    &:hover {
-      transition: 1s ease-in-out;
-      background: #4fda62;
-    }
-  }
-}
+@use '@/assets/styles/not_found.scss';
 </style>
